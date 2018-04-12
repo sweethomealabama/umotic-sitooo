1f42c87412d7721cb4db88f609d40410318aba38
fedmask <33149307+fedmask@users.noreply.github.com>
2018-04-12 10:50:06 +0200

    Update modifica_profilo_Utente.php

--- production/modifica_profilo_Utente.php
+++ production/modifica_profilo_Utente.php
@@ -58,7 +58,7 @@
               </div>
               <div class="profile_info">
                 <span>Benvenuto,</span>
-                <?php echo "<h2> $nomeUtente $cognomeUtente </h2>" ?>
+                
               </div>
             </div>
             <!-- /menu profile quick info -->
