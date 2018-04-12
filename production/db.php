909c2778849ae1db79d2b82e066e72c6b47778f5
fedmask <33149307+fedmask@users.noreply.github.com>
2018-04-12 09:39:55 +0200

    Update db.php

--- production/db.php
+++ production/db.php
@@ -2,8 +2,8 @@
 
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
+	define('DB_', '');
 	define('DB_DATABASE', 'umoticdb');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_,DB_DATABASE);
 
 ?>
