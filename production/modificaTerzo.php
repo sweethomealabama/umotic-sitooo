a1a80a53a194b2012cc316ed3453bada7a1a33ce
sweethomealabama <35634228+sweethomealabama@users.noreply.github.com>
2018-04-15 16:53:15 +0200

    Update modificaTerzo.php

--- production/modificaTerzo.php
+++ production/modificaTerzo.php
@@ -118,7 +118,7 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt=""><?php echo "$nomeUtente $cognomeUtente" ?> 
+                    <img src="images/img.jpg" alt="">
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
